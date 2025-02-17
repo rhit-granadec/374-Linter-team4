beedb04783f240d1a02d73bab8cfa230558acf5e
rhit-granadec <granadec@rose-hulman.edu>
2025-02-17 16:44:14 -0500

    updated duplicate arrow removal

--- LinterProject/src/main/java/org/Main/DuplicateArrowRemover.java
+++ LinterProject/src/main/java/org/Main/DuplicateArrowRemover.java
@@ -8,24 +8,29 @@ public class DuplicateArrowRemover implements ClassContainerOperator{
     @Override
     public void reworkClasses(List<ClassContainer> classList) {
         for (ClassContainer CC: classList) {
+
             ArrayList<ClassContainer.AssociationContainer> assoc = CC.getAssociations();
             int i = 0;
-            ArrayList<Integer> removes = new ArrayList<>();
+            HashSet<Integer> removes = new HashSet<>();
             while(i < assoc.size()) {
                 ClassContainer.AssociationContainer majorAssoc = assoc.get(i);
-                for(int j = i+1; j < assoc.size(); j++) {
+                for(int j = 0; j < assoc.size(); j++) {
+                    if(i == j) continue;
+                    if(removes.contains(i)) continue;
                     ClassContainer.AssociationContainer minorAssoc = assoc.get(j);
                     if(removeAssoc(majorAssoc, minorAssoc)) {
                         removes.add(j);
                     }
                 }
-
-//                while(!removes.isEmpty()){
-//                    int currentPos = removes.pop();
-//                    assoc.remove(currentPos);
-//                }
-
                 i++;
+            }
+
+            Integer[] results = new Integer[removes.size()];
+            removes.toArray(results);
+            Arrays.sort(results);
+
+            for(i = results.length-1; i >=0; i--){
+                assoc.remove((int) results[i]);
             }
 
             CC.setAssociations(assoc);
@@ -34,14 +39,15 @@ public class DuplicateArrowRemover implements ClassContainerOperator{
 
     public boolean removeAssoc(ClassContainer.AssociationContainer majorAssoc,
             ClassContainer.AssociationContainer minorAssoc) {
-        if (majorAssoc.relationshipType == minorAssoc.relationshipType)
-            if (Objects.equals(majorAssoc.AssociationName, minorAssoc.AssociationName)) {
+        if (majorAssoc.relationshipType == minorAssoc.relationshipType
+            || minorAssoc.relationshipType.equals(ClassContainer.relationshipType.DependencyWeak))
+            if (minorAssoc.AssociationName == null || minorAssoc.AssociationName.isEmpty()
+                    || Objects.equals(majorAssoc.AssociationName, minorAssoc.AssociationName)) {
 
                 if (Objects.equals(majorAssoc.ClassName, minorAssoc.ClassName))
                     return true;
-                else if (Objects.equals(majorAssoc.ClassName.replace('/', '.'),
-                        minorAssoc.ClassName.replace('/', '.')))
-                    return true;
+                else return Objects.equals(majorAssoc.ClassName.replace('/', '.'),
+                        minorAssoc.ClassName.replace('/', '.'));
             }
         return false;
     }
