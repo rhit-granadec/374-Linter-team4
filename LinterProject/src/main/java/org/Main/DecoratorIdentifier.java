e7d794e07990048b0197aadd8f2d9d523dfb4547
JJ Henderson <henderae@rose-hulman.edu>
2025-02-22 15:36:34 -0500

    decorator

--- LinterProject/src/main/java/org/Main/DecoratorIdentifier.java
+++ LinterProject/src/main/java/org/Main/DecoratorIdentifier.java
@@ -9,15 +9,26 @@ public class DecoratorIdentifier implements ClassContainerOperator {
     public void reworkClasses(List<ClassContainer> classList) {
         for (ClassContainer cc : classList) {
             ArrayList<String> implementedInterfaces = new ArrayList<>();
+            ArrayList<String> extendedClasses = new ArrayList<>();
             for (ClassContainer.AssociationContainer assoc : cc.getAssociations()) {
                 if (assoc.relationshipType == ClassContainer.relationshipType.Implementation) {
                     implementedInterfaces.add(assoc.ClassName);
                 }
+                if (assoc.relationshipType == ClassContainer.relationshipType.Extension) {
+                    extendedClasses.add(assoc.ClassName);
+                    for (int i = 0; i < classList.size(); i++) {
+                        if (extendedClasses.contains(classList.get(i).getName())) {
+                            for (ClassContainer.AssociationContainer extendAssoc : classList.get(i).getAssociations()) {
+                                if (extendAssoc.relationshipType == ClassContainer.relationshipType.Extension) {
+                                    extendedClasses.add(extendAssoc.ClassName);
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
-
-
-            if (implementedInterfaces.isEmpty()) {
+            if (implementedInterfaces.isEmpty() && extendedClasses.isEmpty()) {
                 continue;
             }
 
@@ -27,13 +38,20 @@ public class DecoratorIdentifier implements ClassContainerOperator {
                     hasComponentField = true;
                     break;
                 }
+                if (extendedClasses.contains(field.returnValue)) {
+                    hasComponentField = true;
+                    break;
+                }
             }
-
             boolean hasComponentInConstructor = false;
             for (ClassContainer.MethodContainer method : cc.getMethods()) {
                 if (method.name.contains("<init>") || method.name.contains("<clinit>")) {
                     for (String paramType : method.inputs) {
                         if (implementedInterfaces.contains(paramType)) {
+                            hasComponentInConstructor = true;
+                            break;
+                        }
+                        if (extendedClasses.contains(paramType)) {
                             hasComponentInConstructor = true;
                             break;
                         }
@@ -55,8 +73,7 @@ public class DecoratorIdentifier implements ClassContainerOperator {
                 }
             }
 
-
-            if (hasComponentField || hasComponentInConstructor ||hasDelegation) {
+            if (hasComponentField || hasComponentInConstructor || hasDelegation) {
                 cc.setDecorator();
             }
         }
